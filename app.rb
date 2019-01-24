882181d01be305ac59bdb4eabb3a36b288bf1549
bendee48 <benbanj@hotmail.com>
2019-01-24 18:03:09 +0000

    Adding new and create routes

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './static/validator.rb'
 
 set :port, 8080
 set :static, true
@@ -10,14 +11,24 @@ def show_names(filename)
   File.read('static/' + filename).split("\n")
 end
 
-#Index
-get '/members/' do
+#Index route
+get '/members' do
   @members = show_names('members.txt')
   erb :index
 end
 
-#Show
+#New route
+get '/members/new' do
+  erb :new_form
+end
+
+#Show route
 get '/members/:name' do
   @name = params[:name]
   erb :show
+end
+
+#Create route
+post '/members' do
+  @name = params[:name]
 end
